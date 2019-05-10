3c28bc835f249af35fe54b0ce3a6b70ca2424d1d
Devon Ward <devoncward@gmail.com>
2019-05-09 18:47:47 -0600

    left half current weather done

--- public/script.js
+++ public/script.js
@@ -15,25 +15,39 @@ window.onload = function() {
 
       //This part below displays the data we have gotten
       let results = ""; //let=creates variable, similar to var keyword
-      results += '<h2>Weather in ' + json.name + "</h2>"; //json.name looks for attribute of name in json file, this could be statically declared then changed
+      results += '<h1 style="float:left;">Weather in ' + json.name + "</h1>"; //json.name looks for attribute of name in json file, this could be statically declared then changed
 
       //Go through weather array, and make a new image for each weather pattern
       //This cannot be statically declared, it must be declared dynamically, because it is an array and we don't know how many will come in from the database
       for (let i=0; i < json.weather.length; i++) {
-	       results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+	       results += '<img style="position:relative" src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
       }
 
-      //Adds the main temperature tag into the h2 file
-      results += '<h2>' + json.main.temp + " &deg;F</h2>"
-
       //Adds the description of the weather
-      results += "<p>"
+      results += "<p>";
+      //Adds the main temperature tag into the h2 file
+      results += '<h2> Av. Temp: ' + json.main.temp + " &deg;F";
+      results += '<ul>';
+        results += '<li>'
+        results += '<h3> High. Temp: ' + json.main.temp_max + " &deg;F</h3>";
+      results += '</li>'
+        results += '<li>'
+        results += '<h3> Low. Temp: ' + json.main.temp_min + " &deg;F</h3>";
+      results += '</li>'
+      results += '</ul>';
+      results += "</h2>";
+      results += '<h2> Humidity: ' + json.main.humidity + " %</h2>";
+      results += '<h2> Wind Speed: ' + json.wind.speed + " mph</h2>";
       for (let i=0; i < json.weather.length; i++) {
-	       results += json.weather[i].description
-	        if (i !== json.weather.length - 1)
-	         results += ", "
+	       results += '<h2>' + json.weather[i].description;
+	        if (i !== json.weather.length - 1) {
+	         results += ", ";
          }
+      }
+      results += "<h2> Cloud Cover: " + json.clouds.all + "%</h2>";
       results += "</p>";
+      results += '<br>';
+      results += "<h1> Recommendation: Perfect weather for a run </h1>";
       document.getElementById("currentWeatherResults").innerHTML = results;
     } catch(err) {
       console.log(err);
@@ -46,7 +60,6 @@ window.onload = function() {
       return;
     console.log(value);
 
-    //const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=cf3be95cf53b4267405bcea8c73696ee";
     const url = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=cf3be95cf53b4267405bcea8c73696ee";
     try {
       const response = await fetch(url);
